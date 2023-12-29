142e47286652533d9ea21f46bb7ad1b1cc09eee3
n3ddu8 <13167147+n3ddu8@users.noreply.github.com>
2023-12-29 12:51:51 +0000

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+import pandas as pd
+
+def my_func():
+  d = {}
+  d["col_a"] = ["a", "b", "c"]
+  d["col_b"] = [1, 2, 3]
+  d["col_c"] = [True, False, None]
+  df = pd.DataFrame.from_dict(d)
+  return df
+
+if __name__ == "__main__":
+
+  print(my_func())
